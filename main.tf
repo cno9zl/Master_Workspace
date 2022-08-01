eb801d3aa5c3967ff0d74bcb0032b42bc32ac57a
WFAUTOMATE <wfautomate@lxcno9zlrh8d01.conseco.ad>
2022-08-01 10:43:01 -0400

    Test_Application_UAT

--- main.tf
+++ main.tf
@@ -5,12 +5,6 @@ terraform {
 
     workspaces {
       name = "Master_Workspace"
-#      execution-mode = "agent"
-#      agent-pool-id = ""
-#      vcs-repo {
-#        identifier = "cno9zl/Test_Application_2_IAC"
-#        oauth-token-id = var.cno9zl_github_token
-#      }
     }
   }
     required_providers {
