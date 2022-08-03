46511fa730d1cc1c1677723d2c412dca71d9b033
WFAUTOMATE <wfautomate@lxcno9zlrh8d01.conseco.ad>
2022-08-03 14:13:54 -0400

    Test_Application_IAC

--- Test_Application_SIT_IAC.tf
+++ Test_Application_SIT_IAC.tf
@@ -34,6 +34,7 @@ resource "tfe_workspace" "Test_Application_SIT" {
   vcs_repo {
   identifier = "cno9zl/Test_Application_SIT_IAC"
   oauth_token_id = resource.tfe_oauth_client.Test_Application_SIT_Oauth.oauth_token_id
+  branch = SIT
   }
 
    lifecycle {
